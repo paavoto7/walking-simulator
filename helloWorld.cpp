fdd1f427dc2ed6e4c172f2f82b58743709dcc1ab
Paavo Westerlund <103736913+paavoto7@users.noreply.github.com>
2025-03-24 12:03:28 +0200

    Camera chapter done

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -8,40 +8,29 @@
 
 #include "stb_image.h"
 #include "shader.h"
+#include "camera.h"
 using namespace std;
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-	glViewport(0, 0, width, height);
-}
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+void processInput(GLFWwindow* window);
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+constexpr int WIDTH{ 800 };
+constexpr int HEIGHT{ 600 };
+
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX{ WIDTH / 2 };
+float lastY{ HEIGHT / 2 };
+bool firstMouse{ true };
+
+float deltaTime{};
+float lastFrame{};
 
 float visibility{ 0.2f };
-float x;
-float y;
-
-void processInput(GLFWwindow* window) {
-	static constexpr float change{ 0.001f };
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-		glfwSetWindowShouldClose(window, true);
-	}
-	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
-		visibility = visibility + change <= 1.0 ? visibility + change : 1.0f;
-	}
-	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
-		visibility = visibility - change >= 0 ? visibility - change : 0.0f;
-	}
-	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
-		y -= change;
-	}
-	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
-		y += change;
-	}
-	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
-		x += change;
-	}
-	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
-		x -= change;
-	}
-}
 
 constexpr float vertices[] = {
 	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
@@ -93,7 +82,7 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", NULL, NULL);
 	if (window == NULL) {
 		cout << "Failed to create GLFW window" << endl;
 		glfwTerminate();
@@ -101,6 +90,9 @@ int main() {
 	}
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		cout << "Failed to initialise GLAD" << endl;
@@ -197,7 +189,21 @@ int main() {
 	shaderProgram.setFloat("visibility", visibility);
 	glEnable(GL_DEPTH_TEST);
 
+	// glm::vec3 cameraPos(0.0f, 0.0f, 3.0f);
+	// 
+	// glm::vec3 cameraTarget(0.0f, 0.0f, 0.0f);
+	// glm::vec3 cameraDirection{ glm::normalize(cameraPos - cameraTarget) };
+	// 
+	// glm::vec3 cameraRight{ glm::normalize(glm::cross(glm::vec3(0.0f, 1.0f, 0.0f), cameraDirection))};
+	// 
+	// glm::vec3 cameraUp{ glm::cross(cameraDirection, cameraRight) };
+
+
 	while (!glfwWindowShouldClose(window)) {
+		float currentFrame{ static_cast<float>(glfwGetTime()) };
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
 		processInput(window);
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -209,10 +215,9 @@ int main() {
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, texture2);
 
-		glm::mat4 view(1.0f);
-		view = glm::translate(view, glm::vec3(x, y, -3.0f));
+		glm::mat4 view{ camera.GetViewMatrix() };
 
-		glm::mat4 projection{ glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f) };
+		glm::mat4 projection{ glm::perspective(glm::radians(camera.Zoom), (float)WIDTH/(float)HEIGHT, 0.1f, 100.0f)};
 		
 		shaderProgram.use();
 		glBindVertexArray(VAO);
@@ -240,4 +245,56 @@ int main() {
 
 	glfwTerminate();
 	return 0;
+}
+
+void processInput(GLFWwindow* window) {
+	static constexpr float change{ 0.001f };
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+		visibility = visibility + change <= 1.0 ? visibility + change : 1.0f;
+	}
+	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+		visibility = visibility - change >= 0 ? visibility - change : 0.0f;
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	}
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	}
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	}
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+	}
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+	if (firstMouse) {
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+	float xoffset{ static_cast<float>(xpos - lastX) };
+	float yoffset{ static_cast<float>(lastY - ypos) };
+	lastX = xpos;
+	lastY = ypos;
+
+	static constexpr float sensitivity{ 0.1f };
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
+	camera.ProcessMouseScroll(static_cast<float>(yoffset));
 }
